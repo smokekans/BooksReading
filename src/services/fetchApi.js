1840fa58d2131a66ad868820bf7bbeebfaad1293
Dmytro Savchenko <smokekans@gmail.com>
2023-03-13 10:53:00 +0200

    update settings

---
+++ src/services/fetchApi.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+axios.defaults.baseURL = 'https://bookread-backend.goit.global';
+
+// const token = {
+//   set(token) {
+//     axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+//   },
+//   unset() {
+//     axios.defaults.headers.common.Authorization = '';
+//   },
+// };
+s;
