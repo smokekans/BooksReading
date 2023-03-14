4899aa2f3e77ae13d0a8c6c07eea7bbc7e57a0f9
Tonya-Ostapiuk <tonya.shevchik@gmail.com>
2023-03-14 13:13:17 +0300

    statistic_to__branch

--- src/pages/Statistics.jsx
+++ src/pages/Statistics.jsx
@@ -1,5 +1,13 @@
+import { CountdounYears } from "components/CountdounYear/CountdounYear";
+import { CountdounGoals } from "components/CountdounGoals/CountdounGoals";
+import { MyGoals } from "components/MyGoalsStatistics/MyGoals";
+
 const Statistics = () => {
-  return <></>;
+  return <>
+  <CountdounYears/>
+  <CountdounGoals/>
+  <MyGoals/>
+  </>;
 };
 
 export default Statistics;
