95197f3e1ff211da6f13c4b509a5d8defdd7aea5
SoloviovO <soloviov.o.m@gmail.com>
2023-03-15 20:56:00 +0200

    add styles for result

--- src/components/StatisticsBookData/StatisticsBookData.jsx
+++ src/components/StatisticsBookData/StatisticsBookData.jsx
@@ -1,5 +1,72 @@
 import React from 'react';
+import {
+  StatDate,
+  StatItem,
+  StatList,
+  StatPage,
+  StatPages,
+  StatTime,
+  StatTitle,
+  StatWrap,
+} from './StatisticsBookData.styled';
 
 export const StatisticsBookData = () => {
-  return <div>StatisticsBookDataList</div>;
+  return (
+    <>
+      <StatWrap>
+        <StatTitle>СТАТИСТИКА</StatTitle>
+        <StatList>
+          <StatItem>
+            <StatDate>15.03.2023</StatDate>
+            <StatTime>08:10:15</StatTime>
+            <StatPages>
+              50&ensp;<StatPage>стор.</StatPage>
+            </StatPages>
+          </StatItem>
+          <StatItem>
+            <StatDate>15.03.2023</StatDate>
+            <StatTime>08:10:15</StatTime>
+            <StatPages>
+              50&ensp;<StatPage>стор.</StatPage>
+            </StatPages>
+          </StatItem>
+          <StatItem>
+            <StatDate>15.03.2023</StatDate>
+            <StatTime>08:10:15</StatTime>
+            <StatPages>
+              50&ensp;<StatPage>стор.</StatPage>
+            </StatPages>
+          </StatItem>
+          <StatItem>
+            <StatDate>15.03.2023</StatDate>
+            <StatTime>08:10:15</StatTime>
+            <StatPages>
+              50&ensp;<StatPage>стор.</StatPage>
+            </StatPages>
+          </StatItem>
+          <StatItem>
+            <StatDate>15.03.2023</StatDate>
+            <StatTime>08:10:15</StatTime>
+            <StatPages>
+              50&ensp;<StatPage>стор.</StatPage>
+            </StatPages>
+          </StatItem>
+          <StatItem>
+            <StatDate>15.03.2023</StatDate>
+            <StatTime>08:10:15</StatTime>
+            <StatPages>
+              50&ensp;<StatPage>стор.</StatPage>
+            </StatPages>
+          </StatItem>
+          <StatItem>
+            <StatDate>15.03.2023</StatDate>
+            <StatTime>08:10:15</StatTime>
+            <StatPages>
+              50&ensp;<StatPage>стор.</StatPage>
+            </StatPages>
+          </StatItem>
+        </StatList>
+      </StatWrap>
+    </>
+  );
 };
