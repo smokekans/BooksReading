aeaa8b3347c3e3a89ff14db29862caa5f8e3afb0
danypryzrak <116654935+danypryzrak@users.noreply.github.com>
2023-03-15 12:40:51 +0100

    OK

--- src/redux/auth/authSlice.js
+++ src/redux/auth/authSlice.js
@@ -1,5 +1,4 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { useNavigate } from 'react-router-dom';
 import { signInThunk, logOutThunk, signUpThunk } from './authOperations';
 
 const authInitialState = {
