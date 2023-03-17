986e5027f5236eacf96c9e33c065041e0f616d73
Serhii Soloviov <115696868+Serhii-Soloviov@users.noreply.github.com>
2023-03-17 11:19:20 +0200

    nfrjjg

--- src/components/ChartLine/ChartLine.js
+++ src/components/ChartLine/ChartLine.js
@@ -9,17 +9,59 @@ import {
   Tooltip,
   Legend,
 } from 'chart.js';
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend
-);
+import {
+  ChartSectionBox,
+  ChartInfoBox,
+  ChartBox,
+  AxisSignatureBox,
+  TitleLineBox,
+  AmountText,
+  AmountValue,
+  TitleLineValue,
+} from './ChartLine.styled';
+// import { useEffect, useState } from 'react';
 
 const ChartLine = () => {
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+  );
+  // const { data } = ({_id: '64142a76d48309d92da85d81', startDate: 1679097600000, finishDate: 1679356800000, owner: '641417233c5e18d54ad0fbe9', book: Array(1), …}
+  // book: [{…}]
+  // finishDate:1679356800000
+  // owner : "641417233c5e18d54ad0fbe9"
+  // startDate:1679097600000
+  // statistic:[]
+  // _id : "64142a76d48309d92da85d81"
+  // [[Prototype]] : Object)
+  // const [statistic, setStatistic] = useState([]);
+  // const [daysLeft, setDaysLeft] = useState(0);
+  // useEffect(() => {
+  //   if (data) {
+  //     setStatistic(data.statistic);
+  //     const { startDate } = data;
+  //     const { finishDate } = data;
+  //     const daysLeft = Math.floor(
+  //       (finishDate - startDate) / (1000 * 60 * 60 * 24)
+  //     );
+  //     setDaysLeft(daysLeft);
+  //   }
+  // }, [data]);
+  // const amountPagesFromStatistic = data?.book?.reduce(
+  //   (totalPages, statisticBookInfo) =>
+  //     totalPages + statisticBookInfo.amountPages,
+  //   0
+  // );
+  // let amountPagesForDay = 0;
+  // if (daysLeft || amountPagesFromStatistic) {
+  //   amountPagesForDay = Math.ceil(amountPagesFromStatistic / daysLeft);
+  // }
+
   const options = {
     responsive: true,
     maintainAspectRatio: false,
@@ -27,7 +69,8 @@ const ChartLine = () => {
       x: {
         grid: {
           color: '#B1B5C2',
-          display:  true,
+          // display: isMobile ? false : true,
+          display: true,
           tickWidth: 0,
           borderColor: '#B1B5C2',
         },
@@ -77,27 +120,54 @@ const ChartLine = () => {
       },
     },
   };
-  const labels = ['start'];
-  const planData = [0];
-  const factData = [0];
-  const data = {
+  // const labels = statistic?.map(item => item.date);
+  // const readPagesFromStatistic = statistic?.map(item => item.amountPages);
+  // const pagesToRead = statistic?.map(item => amountPagesForDay);
+  const labels = [];
+  // const readPagesFromStatistic = [];
+  const pagesToRead = 0;
+  const dataChart = {
     labels,
     datasets: [
       {
         label: 'План',
-        data: planData,
-        borderColor: 'rgb(0, 0, 0)',
-        backgroundColor: 'rgb(0, 0, 0)',
+        //  data: pathname === '/statistics' ? pagesToRead : [],
+        data: [],
+        borderColor: '#091E3F',
+        backgroundColor: '#091E3F',
+        pointRadius: 5,
+        lineTension: 0.3,
       },
       {
         label: 'Факт',
-        data: factData,
+        //  data: pathname === '/statistics' ? readPagesFromStatistic : [],
+        data: pagesToRead,
         borderColor: '#FF6B08',
         backgroundColor: '#FF6B08',
+        pointRadius: 5,
+        lineTension: 0.3,
       },
     ],
   };
-  return <Line options={options} data={data} />;
+  return (
+    <ChartSectionBox>
+      <ChartInfoBox>
+        <AxisSignatureBox>
+          <AmountText>КІЛЬКІСТЬ СТОРІНОК / ДЕНЬ</AmountText>
+          <AmountValue>
+            {/* {pathname === '/statistics' ? amountPagesForDay : 0} */}
+          </AmountValue>
+        </AxisSignatureBox>
+        <TitleLineBox>
+          <TitleLineValue>План</TitleLineValue>
+          <TitleLineValue>Факт</TitleLineValue>
+        </TitleLineBox>
+      </ChartInfoBox>
+      <ChartBox>
+        <Line options={options} data={dataChart} />
+      </ChartBox>
+    </ChartSectionBox>
+  );
 };
 
 export default ChartLine;
