7900fc9b48b38e043877a77bc9ed74f4b4285cc8
Tonya-Ostapiuk <tonya.shevchik@gmail.com>
2023-03-17 14:43:23 +0300

    “myGouls_days”

--- src/components/MyGoalsStatistics/MyGoals.jsx
+++ src/components/MyGoalsStatistics/MyGoals.jsx
@@ -12,14 +12,17 @@ import {
   BoxStl,
   AccentStl,
 } from './MyGoals.styled';
+// import { getDuration } from 'redux/planning/planningSelectors';
 // import { getLastBook } from 'components/StatisticsResultForm/StatisticsResultForm';
 // import { getCurrentlyReading } from 'redux/book/bookSelectors';
+
 export const MyGoals = () => {
   const endDate = new Date(useSelector(getEndDate)).getTime();
+  // const days = useSelector(getDuration);
   const startData = Date.now();
   // const currentBooks = useSelector(getCurrentlyReading);
   
-  const days = Math.floor(((endDate - startData) / (1000 * 60 * 60 * 24)) % 30);
+  const days = Math.floor((endDate - startData) / (1000 * 60 * 60 * 24)) ;
 
   const books = useSelector(getBooksStatistics).length ;
 
