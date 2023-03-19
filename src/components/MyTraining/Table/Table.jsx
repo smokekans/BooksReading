c9a0daa28f39eb9d923f18679556b1a6040b6b21
Maksym-Kovorotnyi <Kovorotny1@gmail.com>
2023-03-19 21:49:45 +0200

    add full adaptive

--- src/components/MyTraining/Table/Table.jsx
+++ src/components/MyTraining/Table/Table.jsx
@@ -1,29 +1,56 @@
 import { useDispatch, useSelector } from "react-redux";
 import { deleteBook } from "redux/planning/planningSlice";
-import { Table, TableBodyItem, TableHeader, TableHeaderItem } from "../MyTraining.styled";
+import { MobileBooksItem, MobileBooksList, MobileBookText, MobileFirstChildContainer, MobileLanguageSwitchBlocks, MobileLanguageSwitchText, MobileTextContainer, Table, TableBodyItem, TableHeader, TableHeaderItem } from "../MyTraining.styled";
 import { ReactComponent as BookIcon } from '../../LibraryTable/svg/bookIconGrey.svg'
 import {ReactComponent as DeleteIcon} from '../svg/delete.svg'
 import { langMyTraining } from "languages/langMyTraining"; 
 import { getLanguage } from "redux/language/languageSelectors";
+import useMatchMedia from "hooks/useMatchMedia";
 
 export const TableComponent = () => {
     const filter = useSelector(state => state.planning.filter);
     const dispatch = useDispatch();
     const lang = useSelector(getLanguage);
-    const {title, author, publishYear, pagesTotal} = langMyTraining
-    // відправка інформації на сервер при кліку на кнопку тренування
+    const { title, author, publishYear, pagesTotal } = langMyTraining
+    const {isMobile} = useMatchMedia()
   
   // видалення книжки
-  const handleDeleteBook = e => {
+    const handleDeleteBook = e => {
+      console.dir(e.currentTarget);
     const removeBook = filter.filter(book => {
       return book._id !== e.currentTarget.parentElement.id;
     });
     dispatch(deleteBook(removeBook));
   };
-
+;
     return (
         <>
-        <Table>
+            {isMobile ? <MobileBooksList>
+                {filter.map(({ _id, title, author, publishYear, pagesTotal }) => {
+                    return <MobileBooksItem key={_id} id={_id}>
+                        <MobileFirstChildContainer id={_id}>
+                        <p>
+                            <BookIcon style={{ marginRight: "14px" }} />
+                            {title}
+                            </p>
+                            <DeleteIcon onClick={handleDeleteBook} />
+                        </MobileFirstChildContainer>
+                        <MobileTextContainer>
+                            <MobileLanguageSwitchBlocks>
+                            <MobileLanguageSwitchText>{langMyTraining.author[lang]}:</MobileLanguageSwitchText>
+                            <MobileLanguageSwitchText>{langMyTraining.publishYear[lang]}:</MobileLanguageSwitchText>
+                            <MobileLanguageSwitchText>{langMyTraining.pagesTotal[lang]}:</MobileLanguageSwitchText>
+                            </MobileLanguageSwitchBlocks>
+                            <div>
+                            <MobileBookText>{author}</MobileBookText>
+                            <MobileBookText>{publishYear}</MobileBookText>
+                            <MobileBookText>{pagesTotal}</MobileBookText>
+                            </div>
+                        </MobileTextContainer>
+                    </MobileBooksItem>
+                })}
+            </MobileBooksList>
+                : <Table>
         <thead>
         <TableHeader>
             <TableHeaderItem>{title[lang]}</TableHeaderItem>
@@ -35,7 +62,7 @@ export const TableComponent = () => {
         <tbody>
           {filter.map(({ _id, title, author, publishYear, pagesTotal }) => {
             return (
-              <tr key={_id} id={_id}>
+              <tr style={{borderTop:'1px solid #E0E5EB', borderBottom:'1px solid #E0E5EB'}} key={_id} id={_id}>
                 <TableBodyItem>
                   <BookIcon style={{ marginRight: "14px" }}/>
                   {title}
@@ -50,10 +77,10 @@ export const TableComponent = () => {
             );
           })}
           <tr>
-            <td><BookIcon style={{ marginRight: "14px" }}/>...</td>
+            <td style={{paddingTop: '12px', paddingBottom:'16px'}}><BookIcon style={{ marginRight: "14px" }}/>...</td>
           </tr>
         </tbody>
-      </Table>
+      </Table>}
         </>
     )
 }
