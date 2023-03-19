6a0f0feebd474a014e522de4a04107b96dfe10ed
Vladyslav Marushchak <marushchakvl@icloud.com>
2023-03-19 21:38:09 +0200

    header fix

--- src/components/Header/Header.styled.js
+++ src/components/Header/Header.styled.js
@@ -8,9 +8,17 @@ export const HeaderWrapper = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding: 0 16px;
+  padding: 0 20px;
   box-shadow: 0px 2px 3px rgba(9, 30, 63, 0.1);
   background-color: #fff;
+
+  @media screen and ${device.tablet} {
+    padding: 0 32px;
+  }
+
+  @media screen and ${device.desktop} {
+    padding: 0 16px;
+  }
 `;
 
 export const Logo = styled.p`
@@ -22,7 +30,7 @@ export const Logo = styled.p`
   font-style: normal;
   font-weight: 400;
   font-size: 20px;
-  line-height: 27px;
+  line-height: calc(27 / 20);
   margin: 0;
 `;
 
@@ -32,19 +40,28 @@ export const Nav = styled.nav`
 
 export const NavList = styled.ul`
   display: flex;
-  margin-right: 20px;
+  margin-right: 10px;
+  justify-content: center;
+  align-items: center;
+
+  @media screen and ${device.tablet} {
+    margin-right: 20px;
+  }
 `;
 
 export const NavItem = styled.li`
   display: flex;
-
   &:first-child {
-    margin-right: 13px;
+    margin-right: 6px;
   }
 
   &:last-child {
     padding-right: 6px;
     border-right: 1px solid #e0e5eb;
+  }
+
+  @media screen and ${device.tablet} {
+    margin-right: 13px;
   }
 `;
 
@@ -63,6 +80,8 @@ export const Link = styled(NavLink)`
 
 export const Icon = styled.svg`
   display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 export const UserPanel = styled.div`
@@ -86,12 +105,16 @@ export const UserName = styled.p`
   font-style: normal;
   font-weight: 300;
   font-size: 14px;
-  line-height: 1.2;
+  line-height: calc(17 / 14);
   color: #242a37;
   margin: 0;
 
   @media screen and ${device.tablet} {
     display: flex;
+  }
+
+  @media screen and ${device.tablet} {
+    line-height: calc(38 / 14);
   }
 `;
 
@@ -100,7 +123,10 @@ export const UserAvatar = styled.p`
   padding 8px 10px;
   border-radius: 50%;
   margin: 0;
-  margin-right: 12px;
+  
+  @media screen and ${device.tablet} {
+   margin-right: 12px;
+ }
 `;
 
 export const ExitButton = styled.button`
@@ -108,7 +134,7 @@ export const ExitButton = styled.button`
   font-style: normal;
   font-weight: 300;
   font-size: 14px;
-  line-height: calc(38 / 14);
+  line-height: calc(17 / 14);
   border: none;
   background-color: #fff;
   text-decoration-line: underline;
@@ -116,5 +142,9 @@ export const ExitButton = styled.button`
 
   &:hover {
     color: #f25137;
+  }
+
+  @media screen and ${device.desktop} {
+    line-height: calc(38 / 14);
   }
 `;
